d075be5f2d00432ae07373f6d1d6ee117b3da9f0
Diego Ramos <rdiego26@gmail.com>
2014-05-17 14:24:25 -0300

    ADD method validate with shake animation

--- src/main/java/alergias/util/Validacoes.java
+++ src/main/java/alergias/util/Validacoes.java
@@ -2,6 +2,9 @@ package alergias.util;
 
 import info.diegoramos.alergiass.R;
 import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 /**
@@ -25,14 +28,18 @@ public class Validacoes {
                     .append(nome_campo).append(" ")
                     .append(msg_obrigatorio);
 
+
+            ToastManager.show(ctx, msg.toString(), 2);
+            /*
 			if(duracao > 1)
 			{
-				Toast.makeText(ctx, msg, Toast.LENGTH_LONG).show();
+                Toast.makeText(ctx, msg, Toast.LENGTH_LONG).show();
 			}
 			else
 			{
 				Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
 			}
+			*/
 				return true;
 		}
 		else
@@ -40,5 +47,30 @@ public class Validacoes {
 			return false;
 		}
 	}
+
+    public final boolean isNullWithAnimation(String nome_campo, Object objeto, View vw, Context ctx)
+    {
+
+        if(objeto.toString().trim().equalsIgnoreCase("") || objeto.toString().trim().equalsIgnoreCase(null) || objeto.toString().trim().equals(null))
+        {
+            Animation shake = AnimationUtils.loadAnimation(ctx, R.anim.shake);
+
+            msg_campo = ctx.getString(R.string.lbl_campo);
+            msg_obrigatorio = ctx.getString(R.string.lbl_obrigatorio);
+            msg = new StringBuilder(msg_campo).append(" ")
+                    .append(nome_campo).append(" ")
+                    .append(msg_obrigatorio);
+
+
+            ToastManager.show(ctx, msg.toString(), 2);
+            vw.startAnimation(shake);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
 }
